af55313e33d31c2519ec76660d16bebb15fd4ca5
ranian <ranian129@gmail.com>
2021-08-24 17:34:40 +0900

    Hello 컨트롤러 코드를 롬복으로 전환

--- src/test/java/com/ranian/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/ranian/book/springboot/web/HelloControllerTest.java
@@ -1,10 +1,9 @@
 package com.ranian.book.springboot.web;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
-public class HelloControllerTest extends TestCase {
+public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
@@ -26,5 +25,19 @@ public class HelloControllerTest extends TestCase {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }

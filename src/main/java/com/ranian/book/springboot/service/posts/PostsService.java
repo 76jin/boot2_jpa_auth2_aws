2da4dbaab10a8c6004a22587765988be14f0c88b
ranian <ranian129@gmail.com>
2021-08-25 17:49:38 +0900

    게시글 수정 API 추가

--- src/main/java/com/ranian/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/ranian/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,9 @@
 package com.ranian.book.springboot.service.posts;
 
+import com.ranian.book.springboot.domain.posts.Posts;
 import com.ranian.book.springboot.domain.posts.PostsRepository;
 import com.ranian.book.springboot.web.dto.PostsSaveRequestDto;
+import com.ranian.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +18,15 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
     }
 }
